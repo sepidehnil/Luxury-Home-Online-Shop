5f43a4d166c650d867ad1fd7ea75f6dc385b687f
sepideh nilforoushan <3pd.nilforoushan@gmail.com>
2023-08-10 13:54:04 -0400

    create dashboard header

---
+++ src/pages/dashboard/Products.jsx
@@ -0,0 +1,12 @@
+function Products() {
+  return (
+    <div>
+      <ul>
+        <li>محصول ۱ </li>
+        <li>محصول ۲</li>
+        <li>محصول ۳</li>
+      </ul>
+    </div>
+  );
+}
+export default Products;

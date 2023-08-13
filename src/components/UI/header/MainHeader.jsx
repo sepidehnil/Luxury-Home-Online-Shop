a5ad0ba0fd352b6a8a3af0ed6b88974e2e4f67df
sepideh nilforoushan <3pd.nilforoushan@gmail.com>
2023-08-13 19:34:33 -0400

    Add search input to header

--- src/components/UI/header/MainHeader.jsx
+++ src/components/UI/header/MainHeader.jsx
@@ -3,11 +3,12 @@ import logo from "../../../assets/images/download.png";
 import carIcon from "../../../assets/svg/cartIcon.svg";
 import userIcon from "../../../assets/svg/userIcon.svg";
 import menueIcon from "../../../assets/svg/menuIcon.svg";
+import search from "../../../assets/svg/search.svg";
 
 function MainHeader() {
   return (
     <header className="font-primary py-[15px] px-[40px] bg-[#c41231] flex items-center justify-between text-white">
-      <div className="flex items-center gap-6">
+      <div className="flex items-center gap-5">
         <div className="w-[190px] h-[60px]">
           <Link to="/home">
             <img src={logo} alt="HomeSense brand logo" />
@@ -19,6 +20,13 @@ function MainHeader() {
             پنل ادمین
           </div>
         </Link>
+        <div className="w-[500px] h-[35px] px-4 bg-slate-200 flex items-center text-black rounded-3xl mr-5">
+          <img src={search} />
+          <input
+            type="text"
+            className="px-[10px] py-[2px] outline-none w-full bg-transparent text-md"
+          />
+        </div>
       </div>
 
       <div className="flex gap-7">

7b39d664f92674ce471df04411de917440283b3f
sepideh nilforoushan <3pd.nilforoushan@gmail.com>
2023-08-13 17:45:01 -0400

    adding menu icon to header

--- src/components/UI/header/MainHeader.jsx
+++ src/components/UI/header/MainHeader.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import logo from "../../../assets/images/download.png";
 import carIcon from "../../../assets/svg/cartIcon.svg";
 import userIcon from "../../../assets/svg/userIcon.svg";
+import menueIcon from "../../../assets/svg/menuIcon.svg";
 
 function MainHeader() {
   return (
@@ -20,18 +21,21 @@ function MainHeader() {
         </Link>
       </div>
 
-      <Link to="/cart">
-        <div className="px-[20px] py-[8px] bg-red-800 bord rounded-3xl flex gap-5 items-center justify-center">
-          <div className="px-[12px] py-[3px] rounded-2xl bg-red-600 flex items-center justify-center text-md">
-            0
-          </div>
+      <div className="flex gap-7">
+        <Link to="/cart">
+          <div className="px-[20px] py-[8px] bg-red-800 bord rounded-3xl flex gap-5 items-center justify-center">
+            <div className="px-[12px] py-[3px] rounded-2xl bg-red-600 flex items-center justify-center text-md">
+              0
+            </div>
 
-          <div className="flex gap-2 text-md">
-            سبد خرید
-            <img src={carIcon} alt="cart icon" />
+            <div className="flex gap-2 text-md">
+              سبد خرید
+              <img src={carIcon} alt="cart icon" />
+            </div>
           </div>
-        </div>
-      </Link>
+        </Link>
+        <img src={menueIcon} />
+      </div>
     </header>
   );
 }

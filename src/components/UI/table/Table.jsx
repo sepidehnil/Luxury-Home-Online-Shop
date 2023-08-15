1b06f95c45db1c994e281799d8adfd6e854c3f99
sepideh nilforoushan <3pd.nilforoushan@gmail.com>
2023-08-15 12:06:30 -0400

    create table

--- src/components/UI/table/Table.jsx
+++ src/components/UI/table/Table.jsx
@@ -1,10 +1,61 @@
-function Table(props) {
-  return (
-    <div className="w-screen m-8 rounded-lg">
-      <div className="bg-white py-6 px-5 rounded-lg flex gap-10">
-        {props.children}
-      </div>
-    </div>
-  );
-}
-export default Table;
+import React from "react";
+import { Space, Table, Tag } from "antd";
+
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: "Age",
+    dataIndex: "age",
+    key: "age",
+  },
+  {
+    title: "Address",
+    dataIndex: "address",
+    key: "address",
+  },
+  {
+    title: "Tags",
+    key: "tags",
+    dataIndex: "tags",
+  },
+  {
+    title: "Action",
+    key: "action",
+    render: (_, record) => (
+      <Space size="middle">
+        <a>Invite {record.name}</a>
+        <a>Delete</a>
+      </Space>
+    ),
+  },
+];
+const data = [
+  {
+    key: "1",
+    name: "John Brown",
+    age: 32,
+    address: "New York No. 1 Lake Park",
+    tags: ["nice", "developer"],
+  },
+  {
+    key: "2",
+    name: "Jim Green",
+    age: 42,
+    address: "London No. 1 Lake Park",
+    tags: ["loser"],
+  },
+  {
+    key: "3",
+    name: "Joe Black",
+    age: 32,
+    address: "Sydney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+];
+const App = () => <Table columns={columns} dataSource={data} />;
+export default App;

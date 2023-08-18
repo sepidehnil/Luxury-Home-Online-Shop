6c83a03fd9e8ee87839ab900eb4da7bccf29daf0
sepideh nilforoushan <3pd.nilforoushan@gmail.com>
2023-08-17 22:09:42 -0400

    final changes to table

--- src/layout/DashBoardLayout.jsx
+++ src/layout/DashBoardLayout.jsx
@@ -6,7 +6,7 @@ import DashBoardSidebar from "../components/UI/modal/DashBoardSidebar";
 function DashBoardLayout() {
   return (
     <Fragment>
-      <div className="flex h-screen w-screen bg-white">
+      <div className="flex h-screen w-screen bg-gray-100">
         <DashBoardSidebar />
         <Outlet />
       </div>
--- src/pages/dashboard/Products.jsx
+++ src/pages/dashboard/Products.jsx
@@ -1,8 +1,11 @@
 import ProductsTable from "../../components/UI/table/ProductsTable";
 function Products() {
   return (
-    <div className="w-[1000px] m-auto font-secondary bg-red-100 rounded-lg ">
-      <ProductsTable />
+    <div className="m-8">
+      <h1 className="font-secondary text-2xl mb-5">مدیریت کالا ها</h1>
+      <div className="w-[1000px]  font-secondary bg-red-100 rounded-lg ">
+        <ProductsTable />
+      </div>
     </div>
   );
 }

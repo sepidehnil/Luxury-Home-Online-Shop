707cb71f130290d5f64363873f8fc7e8b893a090
sepideh nilforoushan <3pd.nilforoushan@gmail.com>
2023-08-17 23:39:45 -0400

    create orders status

--- src/components/UI/table/OrdersTable.jsx
+++ src/components/UI/table/OrdersTable.jsx
@@ -1,4 +1,155 @@
-function OrdersTable() {
-  return <div></div>;
-}
+import React, { useState, useEffect } from "react";
+import { Button, Space, Table } from "antd";
+import axios from "axios";
+
+// const data = [
+//   {
+//     key: "1",
+//     name: "John Brown",
+//     age: 32,
+//     address: "New York No. 1 Lake Park",
+//   },
+//   {
+//     key: "2",
+//     name: "Jim Green",
+//     age: 42,
+//     address: "London No. 1 Lake Park",
+//   },
+//   {
+//     key: "3",
+//     name: "Joe Black",
+//     age: 32,
+//     address: "Sydney No. 1 Lake Park",
+//   },
+//   {
+//     key: "4",
+//     name: "Jim Red",
+//     age: 32,
+//     address: "London No. 2 Lake Park",
+//   },
+// ];
+const OrdersTable = () => {
+  const [data, setData] = useState([]);
+  const [filteredInfo, setFilteredInfo] = useState({});
+
+  const loadUserData = async () => {
+    const resposeProducts = await axios.get(
+      "http://localhost:8000/api/products",
+      {
+        params: { limit: 34 },
+      }
+    );
+    const responseCategories = await axios.get(
+      "http://localhost:8000/api/categories"
+    );
+    const products = resposeProducts.data.data.products;
+    const categories = responseCategories.data.data.categories;
+    const alldatas = products.map((product) => ({
+      ...product,
+      category: categories.find((category) => category._id === product.category)
+        ?.name,
+      price: product.price,
+    }));
+    return alldatas;
+  };
+  useEffect(() => {
+    loadUserData().then((alldatas) => {
+      setData(alldatas);
+    });
+  }, []);
+
+  const handleChange = (pagination, filters) => {
+    console.log("Various parameters", pagination, filters);
+    setFilteredInfo(filters);
+  };
+  const clearFilters = () => {
+    setFilteredInfo({});
+  };
+
+  const columns = [
+    {
+      title: "نام کالا",
+      dataIndex: "name",
+      key: "name",
+      className: "font-secondary",
+    },
+    {
+      title: "دسته بندی",
+      dataIndex: "category",
+      key: "category",
+      className: "font-secondary",
+      filters: [
+        {
+          text: "اتاق خواب",
+          value: "اتاق خواب",
+        },
+        {
+          text: "سالن نشیمن",
+          value: "سالن نشیمن",
+        },
+        {
+          text: "آشپزخانه",
+          value: "آشپزخانه",
+        },
+        {
+          text: "سزویس بهداشتی",
+          value: "سزویس بهداشتی",
+        },
+      ],
+      filteredValue: filteredInfo.category || null,
+      onFilter: (value, record) => record.category.includes(value),
+      ellipsis: true,
+    },
+    {
+      title: "قیمت",
+      dataIndex: "price",
+      key: "price",
+      className: "font-secondary",
+    },
+    {
+      title: "موجودی",
+      dataIndex: "quantity",
+      key: "quantity",
+      className: "w-[150px] ",
+    },
+  ];
+  const paginationConfig = {
+    pageSize: 8,
+  };
+  return (
+    <>
+      <Space
+        style={{
+          marginBottom: 12,
+        }}
+      >
+        <Button onClick={clearFilters} className="bg-white text-black ">
+          Clear filters
+        </Button>
+      </Space>
+      <Table
+        columns={columns}
+        dataSource={data}
+        onChange={handleChange}
+        pagination={paginationConfig}
+        className="font-secondary"
+        components={{
+          header: {
+            cell: ({ children }) => (
+              <th
+                style={{
+                  background: "#ff8e8e",
+                  borderTop: "none",
+                  fontSize: "1rem",
+                }}
+              >
+                {children}
+              </th>
+            ),
+          },
+        }}
+      />
+    </>
+  );
+};
 export default OrdersTable;

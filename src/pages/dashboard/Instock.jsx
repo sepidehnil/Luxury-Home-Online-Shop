4b6791fd975312e7cddc81ef64cd2042aeea5489
sepideh nilforoushan <3pd.nilforoushan@gmail.com>
2023-08-17 22:54:39 -0400

    change the productstable values for instockTable

--- src/pages/dashboard/Instock.jsx
+++ src/pages/dashboard/Instock.jsx
@@ -1,11 +1,11 @@
+import InstockTable from "../../components/UI/table/InstockTable";
 function Instock() {
   return (
-    <div>
-      <ul>
-        <li>محصول ۱ </li>
-        <li>محصول ۲</li>
-        <li>محصول ۳</li>
-      </ul>
+    <div className="m-8">
+      <h1 className="font-secondary text-2xl mb-5">مدیریت موجودی و قیمت ها</h1>
+      <div className="w-[1000px]  font-secondary bg-red-100 rounded-lg ">
+        <InstockTable />
+      </div>
     </div>
   );
 }
